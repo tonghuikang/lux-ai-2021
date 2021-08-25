e4829742dfc37fe9becd545bc24e54b48cb74981
hkmac <tonghuikang@outlook.com>
2021-08-25 12:56:27 +0800

    define setup

---
+++ setup.sh
@@ -0,0 +1,6 @@
+# assuming inside specific conda environment
+
+conda install jupyter ipykernel nb_conda -y
+conda install numpy scipy -y
+conda install requests -y
+pip install kaggle-environments -U
