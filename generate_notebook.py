ed8f6353cd2fd2b68ef801552c4efb8f5d604361
hkmac <tonghuikang@outlook.com>
2021-12-05 10:21:25 -0800

    v110 compare discrepancy

--- generate_notebook.py
+++ generate_notebook.py
@@ -252,7 +252,7 @@ cells.append(nbf.new_code_cell("""\
 %%writefile evaluate_for_map_size.sh
 
 MAP_SIZE=$1
-for run in {1..200};
+for run in {10000001..10000200};
     do GFOOTBALL_DATA_DIR=C lux-ai-2021 --seed $run --loglevel 1 --maxtime 10000 \\
     --height $MAP_SIZE --width $MAP_SIZE --storeReplay=false --storeLogs=false \\
     ./main.py ./ref/main.py >> logs-$MAP_SIZE.txt;
